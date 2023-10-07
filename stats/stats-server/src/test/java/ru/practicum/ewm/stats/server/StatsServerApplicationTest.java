92e637a060b5039fd9a8a21bf545ed317dd7e1ca
Bogomolova <BatAnn04@yandex.ru>
2023-10-08 01:02:05 +0300

    второй коммит

--- stats/stats-server/src/test/java/ru/practicum/ewm/stats/server/StatsServerApplicationTest.java
+++ stats/stats-server/src/test/java/ru/practicum/ewm/stats/server/StatsServerApplicationTest.java
@@ -1,38 +1,14 @@
 package ru.practicum.ewm.stats.server;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-/**
- * Unit test for simple App.
- */
-public class StatsServerApplicationTest
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public StatsServerApplicationTest(String testName )
-    {
-        super( testName );
-    }
+@ActiveProfiles({"test"})
+@SpringBootTest
+class StatsServerApplicationTest {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( StatsServerApplicationTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    void contextLoads() {
     }
 }
