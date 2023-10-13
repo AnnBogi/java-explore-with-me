9bb079474fa9f0045b9bf99b050ac94f9c7b3733
Bogomolova <BatAnn04@yandex.ru>
2023-10-14 01:02:16 +0300

    добавлен сервис

--- main-service/src/main/java/ru/practicum/ewm/mainservice/user/mapper/UserMapper.java
+++ main-service/src/main/java/ru/practicum/ewm/mainservice/user/mapper/UserMapper.java
@@ -1,4 +1,15 @@
 package ru.practicum.ewm.mainservice.user.mapper;
 
-public class UserMapper {
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import ru.practicum.ewm.mainservice.user.dto.UserDto;
+import ru.practicum.ewm.mainservice.user.entity.User;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+
+    @Mapping(target = "id", ignore = true)
+    User fromRequest(UserDto userDto);
+
+    UserDto toDto(User user);
 }
