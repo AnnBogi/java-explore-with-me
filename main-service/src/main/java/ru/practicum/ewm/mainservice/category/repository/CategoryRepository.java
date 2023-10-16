1a80a446eb5c3f3abde8c547247fdf442612f418
Bogomolova <BatAnn04@yandex.ru>
2023-10-16 18:57:03 +0300

    третий коммит

--- main-service/src/main/java/ru/practicum/ewm/mainservice/category/repository/CategoryRepository.java
+++ main-service/src/main/java/ru/practicum/ewm/mainservice/category/repository/CategoryRepository.java
@@ -1,4 +1,13 @@
 package ru.practicum.ewm.mainservice.category.repository;
 
-public class CategoryRepository {
+import org.springframework.data.jpa.repository.Query;
+import ru.practicum.ewm.mainservice.category.entity.Category;
+import ru.practicum.ewm.mainservice.repository.Repository;
+
+import java.util.List;
+
+public interface CategoryRepository extends Repository<Category, Long> {
+
+    @Query(value = "SELECT * FROM categories OFFSET ?1 LIMIT ?2", nativeQuery = true)
+    List<Category> findFrom(int from, int size);
 }
