bc54288ed8d16e905cda486981c4d71e05b1dd3e
Bogomolova <BatAnn04@yandex.ru>
2023-10-21 22:21:01 +0300

    add_utility_class_for_tables_for_JPA v.2

--- main-service/comments/src/main/java/ru/practicum/ewm/mainservice/comment/repository/CommentsTable.java
+++ main-service/comments/src/main/java/ru/practicum/ewm/mainservice/comment/repository/CommentsTable.java
@@ -1,12 +1,12 @@
 package ru.practicum.ewm.mainservice.comment.repository;
 
 public final class CommentsTable {
-  private CommentsTable(){
+  private CommentsTable() {
     //utility class
   }
+
   public static final String ID = "id";
   public static final String AUTHOR = "author";
   public static final String EVENT = "event";
   public static final String COMMENT = "comment";
-
-}
+    }
